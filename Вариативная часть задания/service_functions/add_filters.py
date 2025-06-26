2acdacae1ea01c452ab117ab228f226e498a5039
k0swel <romanleonenko32@gmail.com>
2025-06-26 18:49:41 +0300

    branch for web-site

--- Вариативная часть задания/service_functions/add_filters.py
+++
@@ -1,7 +0,0 @@
-from telebot.async_telebot import AsyncTeleBot
-from telebot.asyncio_filters import StateFilter
-
-# --------------------------------------------- Добавление фильтров ------------------------------------
-def add_filters(bot: AsyncTeleBot):
-    bot.add_custom_filter(StateFilter(bot)) # фильтр с состояниями
-# ----------------------------------------------------------------------------------------------------
